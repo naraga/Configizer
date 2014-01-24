f5d2e0b9057069125ebb0de8a390eead7b918df0
Boris Bucha <naraga@gmail.com>
2014-01-24 20:05:53 +0100

    ConfigurationBase.Detokenize overload

--- src/ConfigizerLib/ConfigurationBase.cs
+++ src/ConfigizerLib/ConfigurationBase.cs
@@ -8,9 +8,14 @@ namespace ConfigizerLib
 
         protected void Detokenize(string path)
         {
+            Detokenize(path, "token");
+        }
+
+        protected void Detokenize(string path, string tokenFileExtension)
+        {
             TextFilesDetokenizer.Detokenize(this, ConfigDirectory,
-                new[] { path }, new []{"*.token"}, 
-                TextFilesDetokenizer.RemoveExtensionOutputFileNameSelector(new []{".token"}));
+                new[] { path }, new[] { "*." + tokenFileExtension },
+                TextFilesDetokenizer.RemoveExtensionOutputFileNameSelector(new[] { "." + tokenFileExtension }));
         }
 
         public virtual void Apply() { }
